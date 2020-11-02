ff0a8921475f836ce6b05eab545736f21063b934
Simen Holmestad <simehol@stud.ntnu.no>
2020-11-02 18:33:00 +0100

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const styles = {
 const Header = () => {
     return (
         <div style={styles}>
-            <header>KART?</header>
+            <header>KART??? Næmmen harru sett!</header>
         </div>
     )
 }
